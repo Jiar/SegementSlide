ea271ec66c76f3d3192c72a517d9d07300c3c04d
Jiar <iiimjiar@gmail.com>
2019-01-23 11:50:37 +0800

    update Example

--- Example/Main/Mine/MineViewController.swift
+++ Example/Main/Mine/MineViewController.swift
@@ -26,6 +26,10 @@ class MineViewController: BaseTransparentSlideViewController {
         return (nil, NSAttributedString(string: "Mine", attributes: UINavigationBar.appearance().titleTextAttributes))
     }
     
+    override var tintColors: DisplayEmbed<UIColor> {
+        return (.black, .black)
+    }
+    
     override var bouncesType: BouncesType {
         return .child
     }
@@ -77,8 +81,27 @@ class MineViewController: BaseTransparentSlideViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "more", style: .plain, target: self, action: #selector(moreAction))
         reloadData()
         scrollToSlide(at: 1, animated: false)
+    }
+    
+    @objc private func moreAction() {
+        let viewController: UIViewController
+        switch Int.random(in: 0..<8) {
+        case 0..<5:
+            viewController = NoticeViewController(selectedIndex: Int.random(in: 0..<2))
+        case 5:
+            viewController = HomeViewController()
+        case 6:
+            viewController = ExploreViewController()
+        case 7:
+            viewController = MineViewController()
+        default:
+            viewController = NoticeViewController(selectedIndex: Int.random(in: 0..<2))
+        }
+        viewController.hidesBottomBarWhenPushed = Bool.random()
+        navigationController?.pushViewController(viewController, animated: true)
     }
 
 }
